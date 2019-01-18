2512fc101dd194dc6cb5d8108afeb1c73c3848b7
Ugur Akin <uakin@sfu.ca>
2019-01-17 18:44:19 -0800

    Initial Commit

---
+++ C++Project/Source/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Tuesday Competitive Doubles Project
+ * 
+ * Desc: Automation of managing, maintaining and displaying
+ * 		 documents related to Tuesday Doubles at Burnaby Tennis Club
+ * 
+ * Author: Ugur Akin
+ * Date: Wednesday, 17 January 2019
+ * 
+ */
+
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+	
+
+	return 0;
+}
